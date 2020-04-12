b870caead522a5f8c4a3c0608633214f8eb9afcc
zfl9 <zfl9.com@gmail.com>
2020-04-12 12:32:58 +0800

    update lrucache.h lrucache.c

--- lrucache.h
+++ lrucache.h
@@ -25,8 +25,9 @@ typedef struct {
 uint16_t lrucache_get_maxsize(void);
 void     lrucache_set_maxsize(uint16_t maxsize);
 
-void udp_socks5ctx_add(udp_socks5ctx_t **cache, udp_socks5ctx_t *entry);
-void udp_tproxyctx_add(udp_tproxyctx_t **cache, udp_tproxyctx_t *entry);
+/* returns the removed key-value pair */
+udp_socks5ctx_t* udp_socks5ctx_add(udp_socks5ctx_t **cache, udp_socks5ctx_t *entry);
+udp_tproxyctx_t* udp_tproxyctx_add(udp_tproxyctx_t **cache, udp_tproxyctx_t *entry);
 
 udp_socks5ctx_t* udp_socks5ctx_get(udp_socks5ctx_t **cache, const ip_port_t *keyptr);
 udp_tproxyctx_t* udp_tproxyctx_get(udp_tproxyctx_t **cache, const ip_port_t *keyptr);
